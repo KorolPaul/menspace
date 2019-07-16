e1fb27da6452f6d1424fecf850c070589ded125d
p_korol <korol.paul@gmail.com>
2019-07-16 18:23:12 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var isMobile = screen.width < 769;
 
     /* Menu toggle */
     const $sidebar = $('.sidebar');
@@ -18,14 +19,16 @@ $(document).ready(function () {
         }
     });
 
-    $('.showcase_logo').on('mousemove', function (e) {
+    $('.showcase_logo-image').on('mousemove', function (e) {
         const { clientHeight, clientWidth } = e.currentTarget;
-        let { layerX, layerY } = e.originalEvent;
+        let { layerX, layerY, offsetX, offsetY } = e.originalEvent;
+        const x = offsetX || layerX;
+        const y = offsetY || layerY;
 
-        const percentX = (layerX / clientWidth * 100) - 50;
-        const percentY = (layerY / clientHeight * 100) - 50;
+        const percentX = (x / clientWidth * 100) - 50;
+        const percentY = (y / clientHeight * 100) - 50;
 
-        $(this).css('transform', `translate(-50%, -50%) rotateY(${percentX / 3}deg) rotateX(${-percentY / 20}deg)`);
+        $(this).css('transform', `rotateY(${percentX / 3}deg) rotateX(${-percentY / 20}deg)`);
     });
 
 
@@ -60,7 +63,12 @@ $(document).ready(function () {
         dots: false,
         items: 3,
         navContainer: $('.gallery_buttons'),
-        navText: ['prev','next']
+        navText: ['prev','next'],
+        //responsive : {
+        //    1024 : {
+        //        items: 6
+        //    }
+        //}
     });
 
     /* Tabs */
@@ -115,13 +123,17 @@ $(document).ready(function () {
         $navigation.eq(activePage).addClass('active');
     }
 
-    document.addEventListener('wheel', scrollPages);
+    if (!isMobile) {
+        document.addEventListener('wheel', scrollPages);
+    }
 
     $navigation.on('click', function(e) {
-        e.preventDefault();
-        var index = $(this).index();
-        activePage = index;
-        scrollPages(false);
+        if (!isMobile) {
+            e.preventDefault();
+            var index = $(this).index();
+            activePage = index;
+            scrollPages(false);
+        }
     });
 
     /* Navigation */
@@ -149,4 +161,23 @@ $(document).ready(function () {
     }
     window.addEventListener('popstate', navigateToPage);
     navigateToPage();
+
+    /* Popup */
+    var $popup = $('.popup');
+    var $popupContent = $('.popup_content');
+
+    function togglePopup() {
+        $popup.toggleClass('opened');
+    }
+
+    $('.service_button').on('click', function(e) {
+        e.preventDefault();
+
+        var content = $(this).next().html();
+        $popupContent.html(content);
+        togglePopup();
+    });
+
+    $('.fade').on('click', togglePopup)
+
 });
